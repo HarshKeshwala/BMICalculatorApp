c313fbd91493dee4a25e1a9a5dc8ee3cdef7c8d8
harshkeshwala <keshwalaharsh@gmail.com>
2018-12-13 20:57:22 -0500

    Basic UI Designing Finished

--- BMICalculatorApp/CalculatorViewController.swift
+++ BMICalculatorApp/CalculatorViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CalculatorViewController.swift
 //  BMICalculatorApp
 //
 //  Created by Harsh Keshwala on 2018-12-13.
@@ -8,17 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalculatorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
